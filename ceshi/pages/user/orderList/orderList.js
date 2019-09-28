84e1583c74e0bdb8df26459340927ff28883998e
wangtianwei-lw <54611182+wangtianwei-lw@users.noreply.github.com>
2019-09-28 19:00:35 +0800

    aaa

---
+++ ceshi/pages/user/orderList/orderList.js
@@ -0,0 +1,101 @@
+// pages/user/orderList/orderList.js
+const app = getApp();
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    Custom: app.globalData.Custom,
+    navTab: ['全部', '待付款', '已支付', '待收货', '待确认'],
+    currentTab: 0,
+  }, 
+  backs: function () {
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+  toOrder: function () {
+    wx.navigateTo({
+      url: '../../store/order/order',
+    })
+  },
+  toOrderDetails: function () {
+    wx.navigateTo({
+      url: '../orderDetails/orderDetails',
+    })
+  },
+  currentTab: function (e) {
+    if (this.data.currentTab == e.currentTarget.dataset.idx) {
+      return;
+    }
+    this.setData({
+      currentTab: e.currentTarget.dataset.idx
+    })
+    this.select = {
+      page: 1,
+      size: 6,
+      isEnd: false
+    }
+    this.data.sendList = [];
+    this.getData()
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
